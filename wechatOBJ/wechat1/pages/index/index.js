d171e95bda2944cfad4c172a8a60b7c6a9ef8460
xioali <1755515486@qq.com>
2019-01-07 20:11:55 +0800

    new file:   wechatOBJ/wechat1/app.js 	new file:   wechatOBJ/wechat1/app.json 	new file:   wechatOBJ/wechat1/app.wxss 	new file:   wechatOBJ/wechat1/pages/images/banner.jpg 	new file:   wechatOBJ/wechat1/pages/images/banner2.jpg 	new file:   wechatOBJ/wechat1/pages/images/timg.jpg 	new file:   wechatOBJ/wechat1/pages/index/index.js 	new file:   wechatOBJ/wechat1/pages/index/index.json 	new file:   wechatOBJ/wechat1/pages/index/index.wxml 	new file:   wechatOBJ/wechat1/pages/index/index.wxss 	new file:   wechatOBJ/wechat1/pages/menu/menu.js 	new file:   wechatOBJ/wechat1/pages/menu/menu.json 	new file:   wechatOBJ/wechat1/pages/menu/menu.wxml 	new file:   wechatOBJ/wechat1/pages/menu/menu.wxss

---
+++ wechatOBJ/wechat1/pages/index/index.js
@@ -0,0 +1,72 @@
+Page({
+     toMenu:function(event){
+       wx.navigateTo({
+         url: '../menu/menu',
+         success: function(res) {},
+         fail: function(res) {},
+         complete: function(res) {},
+       })
+     },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 数据绑定  双引号里边的双花括号包含数据，尽量采用命名规范，在JS文件中编写对应的数据
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
